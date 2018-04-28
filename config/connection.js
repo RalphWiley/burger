a8632141fb3742cc18487dfb1beaaa44a72787e0
RalphWiley <nazkeith@gmail.com>
2018-04-28 12:53:03 -0500

    fourth commit

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,18 @@ var connection;
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection: mysql.createConnection({
-        port: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'burgers_db'
+    connection = mysql.createConnection({
+        host: 's554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'u9oeogrrczye4lf9',
+        password: 'o7wvzj65tuifeojd',
+        database: 'qmim82wg7pf8mi6u'
     });
+    // connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'burgers_db'
+    // });
 }
 
  connection.connect(function(err) {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,26 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var info = {
         brgr: [],
-        itm: []
     };
 
     burger.selectAll(function(data) {
         for(var i = 0; i < data.length; i++) {
             info.brgr.push(data[i]);
         }
-
-        burger.getMenu(function(data) {
-            for(var i = 0; i < data.length; i++) {
-                info.itm.push(data[i]);
-            }
-            res.render('index', info);
-        });
-    });
-});
-
-router.get('/menu', function(req, res) {
-    burger.getMenu(function(data) {
-        res.render('restaurantMenu', { itm : data });
     });
 });
 
